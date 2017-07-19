7b32d4171f4d6f8ab60804d01d8faf804c1d3c44
Michiel Renty <m.renty@gmail.com>
2017-07-19 17:09:00 +0200

    Initial Commit

---
+++ remote/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  remote
+//
+//  Created by Michiel Renty on 19/07/2017.
+//  Copyright © 2017 Michiel Renty. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
